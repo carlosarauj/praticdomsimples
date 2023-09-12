29c79213aa238c05b9585f61364299cf1109feac
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-09-11 21:18:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let p = document.getElementById('p')
+let button = document.getElementById('button')
+
+let button2 = document.getElementById('button2')
+
+function butt(){
+    p.innerText = ('mudou')
+}
+
+function butt2(){
+    p.innerText = ('voltou')
+}
